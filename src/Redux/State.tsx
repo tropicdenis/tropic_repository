9d0851b26ea4b6b47c617c45369073ae579c2fd9
tropicdenis <tropicdenis@gmail.com>
2021-03-12 22:23:44 +0300

    37_SN stopped on 3.09

--- src/Redux/State.tsx
+++ src/Redux/State.tsx
@@ -28,75 +28,80 @@ export type FriendType = {
     avatarURL: string
 }
 
-export type StateType = {
+export type RootStateType = {
     profilePage: ProfilePageType
     dialogsPage: DialogsPageType
     friends: Array<FriendType>
 }
+export type StoreType = {
+    _state: RootStateType
+    rerenderEntireTree: ()=> void
+    addPost: ()=> void
+    updateNewPostText: (newPostText: string) => void
+    subscribe:(observer: ()=> void) => void
 
-let rerenderEntireTree = (props?: any) => {
-    console.log("State changed");
 }
-
-let state: StateType = {
-    profilePage: {
-        posts: [
-            {id: 1, message: "Hi, how are you?", likesCount: 12},
-            {id: 2, message: "It's my first post", likesCount: 11},
-            {id: 3, message: "Blabla", likesCount: 5},
-            {id: 4, message: "Dadada", likesCount: 14}
-        ],
-        newPostText: ""
-    },
-    dialogsPage: {
-        messages: [
-            {id: 1, message: "Hi"},
-            {id: 2, message: "How is your it-kamasutra"},
-            {id: 3, message: "Yo"},
-            {id: 4, message: "Yo"},
-            {id: 5, message: "Yo"},
-        ],
-        dialogs: [
-            {id: 1, name: "Dimych"},
-            {id: 2, name: "Andrey"},
-            {id: 3, name: "Sveta"},
-            {id: 4, name: "Sasha"},
-            {id: 5, name: "Viktor"},
-            {id: 6, name: "Valera"},
+let store: StoreType = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "Hi, how are you?", likesCount: 12},
+                {id: 2, message: "It's my first post", likesCount: 11},
+                {id: 3, message: "Blabla", likesCount: 5},
+                {id: 4, message: "Dadada", likesCount: 14}
+            ],
+            newPostText: ""
+        },
+        dialogsPage: {
+            messages: [
+                {id: 1, message: "Hi"},
+                {id: 2, message: "How is your it-kamasutra"},
+                {id: 3, message: "Yo"},
+                {id: 4, message: "Yo"},
+                {id: 5, message: "Yo"},
+            ],
+            dialogs: [
+                {id: 1, name: "Dimych"},
+                {id: 2, name: "Andrey"},
+                {id: 3, name: "Sveta"},
+                {id: 4, name: "Sasha"},
+                {id: 5, name: "Viktor"},
+                {id: 6, name: "Valera"},
+            ]
+        },
+        friends: [
+            {
+                name: "Garry",
+                avatarURL: "https://nick-intl.mtvnimages.com/uri/mgid:file:gsp:kids-assets:/nick/properties/spongebob-squarepants/characters/gary-character-web-desktop.png?height=0&width=480&matte=true&crop=false"
+            },
+            {name: "Patrick", avatarURL: "https://upload.wikimedia.org/wikipedia/ru/4/4e/Patrick_star-4854.jpg"},
+            {name: "Squidi", avatarURL: "https://www.meme-arsenal.com/memes/8ba9362a677fe74c4e7af0feaeef2360.jpg"}
         ]
     },
-    friends: [
-        {
-            name: "Garry",
-            avatarURL: "https://nick-intl.mtvnimages.com/uri/mgid:file:gsp:kids-assets:/nick/properties/spongebob-squarepants/characters/gary-character-web-desktop.png?height=0&width=480&matte=true&crop=false"
-        },
-        {name: "Patrick", avatarURL: "https://upload.wikimedia.org/wikipedia/ru/4/4e/Patrick_star-4854.jpg"},
-        {name: "Squidi", avatarURL: "https://www.meme-arsenal.com/memes/8ba9362a677fe74c4e7af0feaeef2360.jpg"}
-    ]
+    rerenderEntireTree() {
+        console.log("State changed");
+    },
+    addPost () {
+        const newPost: PostType = {
+            id: new Date().getTime(),
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = ""
+        this.rerenderEntireTree();
+    },
+    updateNewPostText (newPostText) {
+        this._state.profilePage.newPostText = (newPostText);
+        this.rerenderEntireTree();
+    },
+    subscribe (observer) {
+        this.rerenderEntireTree = observer //набллюдатель
+    }
 }
 
 /*window.state = state;*/
-
-export const addPost = (): void => {
-    const newPost: PostType = {
-        id: new Date().getTime(),
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = ""
-    rerenderEntireTree(state);
-}
-export const updateNewPostText = (newPostText: string): void => {
-    state.profilePage.newPostText = (newPostText);
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer: any) => {
-    rerenderEntireTree = observer //набллюдатель
-}
-
-export default state
+export default store
 
 //store - ООП
 
