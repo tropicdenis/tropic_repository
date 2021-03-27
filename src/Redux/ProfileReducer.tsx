8e593cece3381c70c9ca09ac47efbf4d97f382a0
tropicdenis <tropicdenis@gmail.com>
2021-03-27 18:30:42 +0300

    47 is done. Types!.

--- src/Redux/ProfileReducer.tsx
+++ src/Redux/ProfileReducer.tsx
@@ -24,12 +24,16 @@ const profileReducer = (state = initialState, action: ActionsType) => {
                 message: state.newPostText,
                 likesCount: 0
             };
-            state.posts.push(newPost);
-            state.newPostText = ""
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newPostText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText = ""
+            return stateCopy;
+        case UPDATE_NEW_POST_TEXT: {
+            let stateCopy = {...state}
+            stateCopy.newPostText = action.newPostText;
+            return stateCopy;
+        }
         default :
             return state
     }
--- src/index.tsx
+++ src/index.tsx
@@ -6,21 +6,11 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 
-let rerenderEntireTree = (state: any) => {
-    ReactDOM.render(
-        <div>
-            <Provider store={store}>
-            <App />
-            </Provider>
-        </div>,
-        document.getElementById('root')
-    );
-}
-rerenderEntireTree(store.getState());
-
-store.subscribe(()=> {
-    let state = store.getState()
-    rerenderEntireTree(state);
-});
-
-
+ReactDOM.render(
+    <div>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </div>,
+    document.getElementById('root')
+);
