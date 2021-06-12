cc1a74f7894e1e0da1dc05e3e94d7e2f730ec91b
tropicdenis <tropicdenis@gmail.com>
2021-06-12 09:22:33 +0300

    95SN is done. Application deployed.

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import './index.css';
 import store from "./Redux/redux_store";
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>,
+    <Provider store={store}>
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    </Provider>,
     document.getElementById('root')
 );
